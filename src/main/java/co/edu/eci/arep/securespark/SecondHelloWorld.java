3a72cc76a46550e95b76f4ccacfcaf1003c16233
edwar.lozano <edwar.lozano@mail.escuelaing.edu.co>
2023-10-16 17:43:39 -0500

    Funcionando para desplegar en AWS

--- src/main/java/co/edu/eci/arep/securespark/SecondHelloWorld.java
+++ src/main/java/co/edu/eci/arep/securespark/SecondHelloWorld.java
@@ -2,16 +2,16 @@ package co.edu.eci.arep.securespark;
 
 import static spark.Spark.*;
 
-public class HelloWorld {
+public class SecondHelloWorld {
 
-    private static final String SECOND_SERVICE_URL = "https://ec2-34-229-16-130.compute-1.amazonaws.com:9000/hello2";
+    private static final String SECOND_SERVICE_URL = "https://ec2-54-226-41-202.compute-1.amazonaws.com:5000/hello";
 
-    private static final String PATH_FILE = "target/certificados/ecikeystore2.p12";
+    private static final String PATH_FILE = "target/certificados/ecikeystore.p12";
 
     public static void main(String[] args) {
         port(getPort());
         //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath, truststorePassword);
-        secure("target/keystores/ecikeystore.p12", "123456", null, null);
+        secure("target/keystores/ecikeystore2.p12", "123456", null, null);
         get("/hello", (req, res) -> "Hello World, this is the FIRST Hello World service");
         get("/hellosecondservice", (req, res) -> URLReader.main(PATH_FILE, SECOND_SERVICE_URL));
     }
@@ -20,6 +20,6 @@ public class HelloWorld {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+        return 9000; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
